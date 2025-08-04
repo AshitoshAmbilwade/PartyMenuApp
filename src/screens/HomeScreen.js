68b80ca24da017a3d99b05b64b2534ebe68d0d46
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2025-08-04 15:30:23 +0530

    setup done with tailwind config and expo

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+'use dom';
+import '../../global.css'; // adjust path based on your structure
+
+export default function HomeScreen() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <p className="text-2xl font-semibold text-blue-500">
+        Welcome to the Party Menu App 🎉
+      </p>
+    </div>
+  );
+}
